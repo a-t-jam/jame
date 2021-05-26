972a002b69bd29c03b775951596749b69629a955
toyboot4e <toyboot4e@gmail.com>
2021-05-26 21:58:04 +0900

    add coroutine to `combat.go`

--- game/combat/combat.go
+++ game/combat/combat.go
@@ -1,6 +1,8 @@
 package combat
 
 import(
+	"fmt"
+
 	_ "image/png"
 	_ "image/jpeg"
 
@@ -8,11 +10,55 @@ import(
 	"github.com/a-t-jam/jame/game/scene"
 )
 
+var(
+    bg *ebiten.Image
+)
+
+// FIXME: duplicate image loading
+func init() {
+	bg = scene.LoadImg("winddorf/kyoto.jpg")
+}
+
+type CombatState struct {
+	actors [10]scene.Combat
+}
+
+var (
+	resume <-chan bool
+	state CombatState
+)
+
+// genTurnState returns a coroutine implemented as a channel that holds turn-based game state.
+//
+// Returns if we should continue the combat scene.
+func genTurnState(state *CombatState) chan bool {
+	yield := make (chan bool);
+
+	go func() {
+		defer close(yield)
+		for i := 1; i < 10; i++ {
+			yield <- true;
+		}
+	}()
+
+	return yield
+}
+
+// Enter initializes the combat scene
+func Enter(scene *scene.Scene) {
+	// TODO: overwrite `state` with `scene.ducks`
+	state = CombatState {}
+	resume = genTurnState(&state)
+}
+
 func Update(scene *scene.Scene) error {
+	running := <-resume
+	if !running {
+		// TODO: it's over. pop the combat scene
+	}
 	return nil
 }
 
 func Draw(scene *scene.Scene, screen *ebiten.Image) {
 	//
 }
-
