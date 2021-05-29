5c645a0fb22e7a75a34cb1227d1cb22ff7ba5832
toyboot4e <toyboot4e@gmail.com>
2021-05-29 10:44:31 +0900

    use ocean1 as background for travel secne

--- game/travel/travel.go
+++ game/travel/travel.go
@@ -1,14 +1,6 @@
 package travel
 
 import (
-	"bytes"
-	_ "embed"
-	"image"
-	"log"
-
-	_ "image/jpeg"
-	_ "image/png"
-
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 
@@ -17,39 +9,11 @@ import (
 )
 
 var (
-	bg           *ebiten.Image
 	playerSprite *ebiten.Image
 )
 
 func init() {
-	// byte data
-	imgByte, err := assets.Data.ReadFile("winddorf/kyoto.jpg")
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	// std image
-	img, _, err := image.Decode(bytes.NewReader(imgByte))
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	ebitenImg := ebiten.NewImageFromImage(img)
-	bg = ebitenImg
-
-	// testing sprites
-	imgByte, err = assets.Data.ReadFile("sprites/amg1_rt2.png")
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	img, _, err = image.Decode(bytes.NewReader(imgByte))
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	ebitenImg = ebiten.NewImageFromImage(img)
-	playerSprite = ebitenImg
+	playerSprite = assets.LoadImg("sprites/amg1_rt2.png")
 }
 
 func Update(scene *scene.Scene) error {
@@ -58,13 +22,12 @@ func Update(scene *scene.Scene) error {
 
 func Draw(scene *scene.Scene, screen *ebiten.Image) {
 	ebitenutil.DebugPrint(screen, "Hello, World!")
-	op := &ebiten.DrawImageOptions{}
-	op1 := &ebiten.DrawImageOptions{}
-	op.GeoM.Translate((1280-790)/2, (720-480)/2)
-	screen.DrawImage(bg, op)
+
+	assets.DrawOcean1(screen)
 
 	// sprite
-	op1.GeoM.Translate(50, 100)
-	op1.GeoM.Scale(2, 2)
-	screen.DrawImage(playerSprite, op1)
+	op := ebiten.DrawImageOptions{}
+	op.GeoM.Translate(50, 100)
+	op.GeoM.Scale(2, 2)
+	screen.DrawImage(playerSprite, &op)
 }
